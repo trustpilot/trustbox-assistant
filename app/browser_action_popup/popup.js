ee45fbce93e05fe3a832ffe6f5b35c789f37e553
Miklos Aubert <mat@trustpilot.com>
2017-07-28 16:56:49 +0200

    Adding SEO data

--- app/browser_action_popup/popup.js
+++ app/browser_action_popup/popup.js
@@ -5,14 +5,32 @@ const contentScriptCallback = (results) => {
   }
 
   try {
+    const renderBoolean = (value) => {
+      return value ? '✅' : '❌';
+    };
+
     const boostrapCheck = document.getElementById('bootstrapFromTrustpilot');
-    boostrapCheck.innerHTML = result.bootstrapFromTrustpilot ? '✅' : '❌';
+    boostrapCheck.innerHTML = renderBoolean(result.bootstrapFromTrustpilot);
 
     const inHeadCheck = document.getElementById('bootstrapInHead');
-    inHeadCheck.innerHTML = result.bootstrapInHead ? '✅' : '❌';
+    inHeadCheck.innerHTML = renderBoolean(result.bootstrapInHead);
 
     const widgetStats = document.getElementById('widgetStats');
     widgetStats.innerHTML = `${result.numLiveWidgets} / ${result.numWidgets}`;
+
+    const richSnippetsWidgets = document.getElementById('numRichSnippetsWidgets');
+    richSnippetsWidgets.innerHTML = result.numRichSnippetsWidgets;
+
+    const richSnippets = document.getElementById('richSnippets');
+    richSnippets.innerHTML = result.numRichSnippetsWidgets ? renderBoolean(result.orgRichSnippet) : 'N/A';
+
+    const productRichSnippetsWidgets = document.getElementById('numProductRichSnippetsWidgets');
+    productRichSnippetsWidgets.innerHTML = result.numProductRichSnippetsWidgets;
+
+    const productRichSnippets = document.getElementById('productRichSnippets');
+    productRichSnippets.innerHTML =
+      result.numProductRichSnippetsWidgets ? renderBoolean(result.productRichSnippet) : 'N/A';
+
   } catch (e) {
     console.debug(e);
   }
--- app/content_scripts/trustbox-checker.js
+++ app/content_scripts/trustbox-checker.js
@@ -6,12 +6,42 @@
   const bootstrapInHead = bootstrap && bootstrap.parentElement.tagName === 'HEAD';
   const widgets = Array.from(document.querySelectorAll('.trustpilot-widget'));
   const liveWidgets = widgets.filter((w) => w.firstChild && w.firstChild.tagName === 'IFRAME');
+  const richSnippetsWidgets = widgets.filter((w) => w.dataset.schemaType === 'Organization');
+  const productRichSnippetsWidgets = widgets.filter((w) => w.dataset.sku && w.dataset.name);
+
+  const richSnippets = Array.from(document.getElementsByTagName('script')).filter((s) => {
+    return s.type && s.type === 'application/ld+json';
+  });
+
+  const orgRichSnippet = richSnippets.filter((s) => {
+    try {
+      const snippet = JSON.parse(s.innerText);
+      return snippet['@type'] === 'Organization' && snippet.aggregateRating;
+    } catch (e) {
+      console.debug(e);
+    }
+    return false;
+  }).pop();
+
+  const productRichSnippet = richSnippets.filter((s) => {
+    try {
+      const snippet = JSON.parse(s.innerText);
+      return snippet['@type'] === 'Product' && snippet.review;
+    } catch (e) {
+      console.debug(e);
+    }
+    return false;
+  }).pop();
 
   return {
     bootstrapFromTrustpilot: !!bootstrap,
     bootstrapInHead,
     numWidgets: widgets.length,
-    numLiveWidgets: liveWidgets.length
+    numLiveWidgets: liveWidgets.length,
+    numRichSnippetsWidgets: richSnippetsWidgets.length,
+    numProductRichSnippetsWidgets: productRichSnippetsWidgets.length,
+    orgRichSnippet: !!orgRichSnippet,
+    productRichSnippet: !!productRichSnippet
   };
 
 })(window, document);
