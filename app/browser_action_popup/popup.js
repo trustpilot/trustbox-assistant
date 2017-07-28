9696e8fe25adaf82d78493b4c0c7b32cf0faa275
Miklos Aubert <mat@trustpilot.com>
2017-07-28 13:54:18 +0200

    More work on the popup

--- app/browser_action_popup/popup.js
+++ app/browser_action_popup/popup.js
@@ -1,3 +1,22 @@
 browser.tabs.executeScript({
   file: '/app/trustbox-checker.js'
+}).then((results) => {
+  const result = results.pop();
+  if (!result) {
+    return;
+  }
+
+  try {
+    const boostrapCheck = document.getElementById('bootstrapFromTrustpilot');
+    boostrapCheck.innerHTML = result.bootstrapFromTrustpilot ? '✅' : '❌';
+
+    const inHeadCheck = document.getElementById('bootstrapInHead');
+    inHeadCheck.innerHTML = result.bootstrapInHead ? '✅' : '❌';
+
+    const widgetStats = document.getElementById('widgetStats');
+    widgetStats.innerHTML = `${result.numLiveWidgets} / ${result.numWidgets}`;
+  } catch (e) {
+    console.debug(e);
+  }
+
 });
--- app/trustbox-checker.js
+++ app/trustbox-checker.js
@@ -1,9 +1,17 @@
 ((window, document) => {
 
-  window.TrustBoxChecker = window.TrustBoxChecker || {
-    queryWidgets: () => {
-      return Array.from(document.querySelectorAll('.trustpilot-widget'));
-    }
+  const bootstrap = Array.from(document.getElementsByTagName('script')).filter((s) => {
+    return s.src.endsWith('//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js');
+  }).pop();
+  const bootstrapInHead = bootstrap && bootstrap.parentElement.tagName === 'HEAD';
+  const widgets = Array.from(document.querySelectorAll('.trustpilot-widget'));
+  const liveWidgets = widgets.filter((w) => w.firstChild && w.firstChild.tagName === 'IFRAME');
+
+  return {
+    bootstrapFromTrustpilot: !!bootstrap,
+    bootstrapInHead,
+    numWidgets: widgets.length,
+    numLiveWidgets: liveWidgets.length
   };
 
 })(window, document);
